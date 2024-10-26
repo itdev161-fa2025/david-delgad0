dbb6c628f8a46ae37f9abf8dd853a578532d615f
David Delgado <delgaddi@gmatc.matc.edu>
2024-10-26 00:41:00 -0500

    Completed Activity 5

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
-import cors from `cors`;
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from './models/User'
 
 // Initialize express application
 const app = express();
@@ -11,11 +15,11 @@ connectDatabase();
 
 // Configure Middleware
 app.use(express.json({ extended: false }));
-app.use(cors({origin: `http://localhost3000`}));
+app.use(cors({ origin: 'http://localhost:3000'}));
 
 // API endpoints
 app.get('/', (req, res) => 
-    res.send('HTTP GET request sent to root API endpoint')
+    res.send('http get request sent to root api endpoint')
 );
 
 /**
@@ -33,15 +37,58 @@ app.post('/api/users',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     } else {
-      return res.send(req.body);
+      const { name, email, password } = req.body;
+      try {
+        let user = await User.findOne({ email: email });
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'User already exists'}] });
+        }
+
+        // Create a new user
+        user = new User({
+          name: name,
+          email: email,
+          password: password
+        });
+
+        // Encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Save to the db and return
+        await user.save();
+        
+        // Generate and return a JWT token
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
+
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: '10hr' },
+          (err, token) => {
+            if (err) {
+              throw (err);              
+            }
+            res.json({ token: token });
+          }
+        );
+      } catch (error) {
+        res.status(500).send('Server error');
+      }
     }
-  }
-);
+});
+
 
 // Connection listener
 const port = 5000;
